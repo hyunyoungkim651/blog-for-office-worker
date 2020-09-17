e6726ec639d26b9e2804d5bc1fb980b80149aeb3
hyunyoungkim651 <k00810111@gmail.com>
2020-09-17 18:49:17 +0900

    2020-09-17

--- feed/views.py
+++ feed/views.py
@@ -2,11 +2,37 @@ from django.shortcuts import render
 from .models import Article, Comment, HashTag
 # Create your views here.
 def index(request):
-    article_list = Article.objects.all()
+
+    # GET & POST 방식
+    category = request.GET.get("category")
+
+
+    if not category:
+        article_list = Article.objects.all()
+    else:
+        article_list = Article.objects.filter(category = category)
+
     hashtag_list = HashTag.objects.all()
+
+    # category_list = set([])
+    # for article in article_list:
+    #     category_list.add(article.get_category_display())
+    # print(category_list)
+
+    # category_list = set([
+    #     article.get_category_display()
+    #     for article in article_list
+    # ])
+
+    category_list = set([
+        (article.category, article.get_category_display())
+        for article in article_list
+    ])
+
     ctx = {
         "article_list" : article_list,
-        "hashtag_list" : hashtag_list
+        "hashtag_list" : hashtag_list,
+        "category_list" : category_list,
     }
     return render(request, "index.html", ctx)
 
